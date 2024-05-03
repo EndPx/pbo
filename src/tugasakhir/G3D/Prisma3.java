622c3ebb1f7b53ed17e5e0f35858ffd29800e49a
EndPx <127120051+EndPx@users.noreply.github.com>
2024-05-03 23:45:38 +0700

    Update Prisma3.java

--- src/tugasakhir/G3D/Prisma3.java
+++ src/tugasakhir/G3D/Prisma3.java
@@ -5,10 +5,41 @@
  */
 package tugasakhir.G3D;
 
+import tugasakhir.Benda3D;
+import tugasakhir.G2D.Segitiga;
+
 /**
  *
  * @author abaym
  */
-public class Prisma3 {
-    
+public class Prisma3 extends Segitiga implements Benda3D {
+
+    private double tinggiPrisma;
+    public Prisma3(double alas, double tinggi, double tinggiPrisma) {
+        super(alas, tinggi);
+        this.tinggiPrisma = tinggiPrisma;
+    }
+
+    @Override
+    public double menghitungVolume() {
+        double luasAlas = menghitungLuas();
+        return menghitungLuas() * tinggiPrisma;
+    }
+
+    public double menghitungVolume(double luasAlas) {
+        return luasAlas * tinggiPrisma;
+    }
+
+    @Override
+    public double menghitungLuasPermukaan() {
+        double luasSegitigaSisiTegak = 0.5 * super.getAlas() * tinggiPrisma; // Menghitung luas segitiga sisi tegak
+        double luasSelimut = luasSegitigaSisiTegak * 3;
+        return  super.menghitungLuas() + luasSelimut;
+    }
+
+    public double menghitungLuasPermukaan(double luasAlas) {
+        double luasSegitigaSisiTegak = 0.5 * super.getAlas() * tinggiPrisma;
+        double luasSelimut = luasSegitigaSisiTegak * 3;
+        return luasAlas + luasSelimut;
+    }
 }
