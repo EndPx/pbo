377fda4fb40cec8ae7a65756aacb614fa9919e91
RafiqIkhwan <90625702+RafiqIkhwan@users.noreply.github.com>
2024-05-04 00:56:48 +0700

    Update LimasP.java
    menambah baris code untuk menghitung limas persegi

--- src/tugasakhir/G3D/LimasP.java
+++ src/tugasakhir/G3D/LimasP.java
@@ -5,10 +5,49 @@
  */
 package tugasakhir.G3D;
 
+import tugasakhir.Benda3D;
+import tugasakhir.G2D.Persegi;
+
 /**
  *
  * @author abaym
  */
-public class LimasP {
-    
+public class LimasP extends Persegi implements Benda3D {
+
+    private double tinggiLimas;
+
+    public LimasP(double sisi, double tinggiLimas) {
+        super(sisi);
+        this.tinggiLimas = tinggiLimas;
+    }
+
+    @Override
+    public double menghitungVolume() {
+        return (1.0 / 3) * menghitungLuas() * tinggiLimas;
+    }
+
+    public double menghitungVolume(double luasAlas) {
+        return (1.0 / 3) * luasAlas * tinggiLimas;
+    }
+
+    @Override
+    public double menghitungLuasPermukaan() {
+        double luasAlas = super.menghitungLuas();
+        double kelilingAlas = 4 * super.getSisi();
+        double luasSelimut = 0.5 * kelilingAlas * getSisiTegak();
+        double luasPermukaan = luasAlas + luasSelimut;
+        return luasPermukaan;
+    }
+
+    public double menghitungLuasPermukaan(double luasAlas) {
+        double kelilingAlas = 4 * super.getSisi();
+        double luasSelimut = 0.5 * kelilingAlas * getSisiTegak();
+        double luasPermukaan = luasAlas + luasSelimut;
+        return luasPermukaan;
+    }
+
+    private double getSisiTegak() {
+        double sisiTegak = Math.sqrt(Math.pow(super.getSisi(),2) + Math.pow(tinggiLimas,2));
+        return sisiTegak;
+    }
 }
