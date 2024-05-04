f5622a4bca8d649e10f082e0ad3d0e71602bb521
kaptensifu <150881290+kaptensifu@users.noreply.github.com>
2024-05-04 14:14:37 +0700

    Update PrismaPP.java

--- src/tugasakhir/G3D/PrismaPP.java
+++ src/tugasakhir/G3D/PrismaPP.java
@@ -4,11 +4,49 @@
  * and open the template in the editor.
  */
 package tugasakhir.G3D;
-
+import tugasakhir.Benda3D;
+import tugasakhir.G2D.PersegiPanjang;
 /**
  *
  * @author abaym
  */
-public class PrismaPP {
+public class PrismaPP extends PersegiPanjang implements Benda3D{
+
+    private double tinggiPrismaPP;
+    
+    public PrismaPP(double panjang,double lebar,double tinggiPrismaPP) {
+        super(panjang,lebar);
+        this.tinggiPrismaPP = tinggiPrismaPP;
+    }
+
+    @Override
+    public double menghitungVolume() {
+        return menghitungLuas() * tinggiPrismaPP;
+    }
+    
+    public double menghitungVolume(double luasAlas){
+        return luasAlas * tinggiPrismaPP;
+    }
+
+    @Override
+    public double menghitungLuasPermukaan() {
+        double luasAlas = super.menghitungLuas();
+        double luasSelimut1 = 2 * luasAlas;
+        double luasSamping = super.getLebar() * tinggiPrismaPP;
+        double luasSelimut2 = 2 * luasSamping;
+        double luasDepan = super.getPanjang() * tinggiPrismaPP;
+        double luasSelimut3 = 2 * luasDepan;
+        return luasSelimut1 + luasSelimut2 + luasSelimut3;
+    }
+
+    public double menghitungLuasPermukaan(double luasAlas) {
+        double luasAlas = super.menghitungLuas();
+        double luasSelimut1 = 2 * luasAlas;
+        double luasSamping = super.getLebar() * tinggiPrismaPP;
+        double luasSelimut2 = 2 * luasSamping;
+        double luasDepan = super.getPanjang() * tinggiPrismaPP;
+        double luasSelimut3 = 2 * luasDepan;
+        return luasSelimut1 + luasSelimut2 + luasSelimut3;
+    }
     
 }
