06965e5a7a1f8a759e6d23533ddf5136df6e56ca
kaptensifu <150881290+kaptensifu@users.noreply.github.com>
2024-05-04 15:01:54 +0700

    Update Kerucut.java

--- src/tugasakhir/G3D/Kerucut.java
+++ src/tugasakhir/G3D/Kerucut.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package tugasakhir.G3D;
-
+import tugasakhir.Benda3D;
+import tugasakhir.G2D.Lingkaran;
 /**
  *
  * @author abaym
  */
-public class Kerucut {
+public class Kerucut extends Lingkaran implements Benda3D {
+
+    private double tinggiKerucut;
     
+    public Tabung(double jariJari, double tinggiKerucut) {
+        super(jariJari);
+        this.tinggiKerucut = tinggiKerucut;
+    }
+
+     @Override
+    public double menghitungVolume() {
+        return (1.0/3) * menghitungLuas() * tinggiTabung;
+    }
+
+    public double menghitungVolume(double luasAlas) {
+        return (1.0/3) * luasAlas * tinggiTabung;
+    }
+
+    public double menghitungLuasPermukaan() {
+        double sisiMiring = Math.sqrt(Math.pow(super.getJariJari(),2) + Math.pow(tinggiTabung,2));
+        double luasSelimut = Math.PI * super.getJariJari() * sisiMiring;
+        return super.menghitungLuas() + luasSelimut;
+    }
+
+    public double menghitungLuasPermukaan(double luasAlas) {
+        double sisiMiring = Math.sqrt(Math.pow(super.getJariJari(),2) + Math.pow(tinggiTabung,2));
+        double luasSelimut = Math.PI * super.getJariJari() * sisiMiring;
+        return luasAlas + luasSelimut;
+    }
 }
