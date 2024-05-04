48883c989dff13c1c0c75991e96551a7cf3ac06e
kaptensifu <150881290+kaptensifu@users.noreply.github.com>
2024-05-04 14:39:13 +0700

    Update Tabung.java

--- src/tugasakhir/G3D/Tabung.java
+++ src/tugasakhir/G3D/Tabung.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package tugasakhir.G3D;
-
+import tugasakhir.Benda3D;
+import tugasakhir.G2D.Lingkaran;
 /**
  *
  * @author abaym
  */
-public class Tabung {
+public class Tabung extends Lingkaran implements Benda3D {
+
+    private double tinggiTabung;
+    
+    public Tabung(double jariJari, double tinggiTabung) {
+        super(jariJari);
+        this.tinggiTabung = tinggiTabung;
+    }
+
+    @Override
+    public double menghitungVolume() {
+        return menghitungLuas() * tinggiTabung;
+    }
+
+    public double menghitungVolume(double luasAlas) {
+        return luasAlas * tinggiTabung;
+    }
+
+    @Override
+    public double menghitungLuasPermukaan() {
+        double luasPermukaan = 2 * Math.PI * super.getJariJari() * (super.getJariJari() + tinggiTabung);
+        return luasPermukaan;
+    }
+
+    public double menghitungLuasPermukaan(double luasAlas) {
+        double luasPermukaan = 2 * Math.PI * super.getJariJari() * (super.getJariJari() + tinggiTabung);
+        return luasPermukaan;
+    }
     
 }
