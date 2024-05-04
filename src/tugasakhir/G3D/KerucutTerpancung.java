3c51b2a4a26a1f3c5504a7acb3efa095721aa301
kaptensifu <150881290+kaptensifu@users.noreply.github.com>
2024-05-04 15:25:37 +0700

    Update KerucutTerpancung.java

--- src/tugasakhir/G3D/KerucutTerpancung.java
+++ src/tugasakhir/G3D/KerucutTerpancung.java
@@ -4,11 +4,42 @@
  * and open the template in the editor.
  */
 package tugasakhir.G3D;
-
+import tugasakhir.Benda3D;
+import tugasakhir.G2D.Lingkaran;
 /**
  *
  * @author abaym
  */
-public class KerucutTerpancung {
+public class KerucutTerpancung extends Lingkaran implements Benda3D {
+
+    private double tinggiKerucutT;
+    private double jarijariKecil;
     
+    public Tabung(double jariJari, double tinggiKerucutT, double jarijariKecil) {
+        super(jariJari);
+        this.tinggiKerucutT = tinggiKerucutT;
+        this.jarijariKecil = jarijariKecil;
+    }
+
+    @Override
+    public double menghitungVolume() {
+        return (1.0/3) * Math.PI * tinggiKerucutT * ((Math.pow(super.getJariJari(),2)) + (super.getJariJari() * jarijariKecil) + (Math.pow(jarijariKecil,2)));
+    }
+
+    public double menghitungVolume(double luasAlas) {
+        return (1.0/3) * Math.PI * tinggiKerucutT * ((Math.pow(super.getJariJari(),2)) + (super.getJariJari() * jarijariKecil) + (Math.pow(jarijariKecil,2)));
+    }
+
+    @Override
+    public double menghitungLuasPermukaan() {
+        double sisiMiring = Math.sqrt(Math.pow(super.getJariJari() - jarijariKecil,2) + Math.pow(tinggiKerucutT,2));
+        double luasPermukaan = Math.PI * sisiMiring  * (super.getJariJari() + jarijariKecil);
+        return luasPermukaan;
+    }
+
+    public double menghitungLuasPermukaan(double luasAlas) {
+        double sisiMiring = Math.sqrt(Math.pow(super.getJariJari() - jarijariKecil,2) + Math.pow(tinggiKerucutT,2));
+        double luasPermukaan = Math.PI * sisiMiring  * (super.getJariJari() + jarijariKecil);
+        return luasPermukaan;
+    }
 }
