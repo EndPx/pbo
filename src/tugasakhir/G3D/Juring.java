e1067c9fbf95da530f92d4d1c59a27f909414f04
EndPx <127120051+EndPx@users.noreply.github.com>
2024-05-07 06:40:09 +0700

    Update Juring.java

--- src/tugasakhir/G3D/Juring.java
+++ src/tugasakhir/G3D/Juring.java
@@ -5,47 +5,32 @@
  */
 package tugasakhir.G3D;
 import tugasakhir.Benda3D;
-import tugasakhir.G2D.Lingkaran;
 /**
  *
  * @author abaym
  */
-public class Juring extends Lingkaran implements Benda3D {
-    public Juring(double jariJariBawah, double jariJariAtas, double tinggi) {
-        super(jariJariBawah);
-        this.jariJariAtas = jariJariAtas;
-        this.tinggi = tinggi;
+public class Juring extends Bola implements Benda3D {
+    private double sudut;
+    public Juring(double jariJari, double sudut) {
+        super(jariJari);
+        this.sudut = sudut;
     }
-
-    private double jariJariAtas;
-    private double tinggi;
-
-    public double getJariJariAtas() {
-        return jariJariAtas;
-    }
-
-    public void setJariJariAtas(double jariJariAtas) {
-        this.jariJariAtas = jariJariAtas;
-    }
-
-    public double getTinggi() {
-        return tinggi;
-    }
-
-    public void setTinggi(double tinggi) {
-        this.tinggi = tinggi;
-    }
-
+    
     @Override
     public double menghitungVolume() {
-        return (1.0 / 3.0) * Math.PI * tinggi * (Math.pow(getJariJari(), 2) + Math.pow(getJariJariAtas(), 2) + (getJariJari() * getJariJariAtas()));
+        return (sudut/360) * super.menghitungVolume();
+    }
+    
+    public double menghitungVolume(double volumeBola) {
+        return (sudut/360) * volumeBola;
     }
 
     @Override
     public double menghitungLuasPermukaan() {
-        double r = getJariJari();
-        double R = getJariJariAtas();
-        double l = Math.sqrt(tinggi * tinggi + (R - r) * (R - r));
-        return Math.PI * (r + R) * l + Math.PI * r * r;
+        return (sudut/360) * super.menghitungLuasPermukaan();
+    }
+    
+    public double menghitungLuasPermukaan(double luasPermukaanBola) {
+        return (sudut/360) * luasPermukaanBola;
     }
 }
