5c880b16f30d8e51a313630f7d38628541b82358
Fiuw Winlye <142503944+RusterF@users.noreply.github.com>
2024-05-06 16:57:19 +0700

    Update Bola.java

--- src/tugasakhir/G3D/Bola.java
+++ src/tugasakhir/G3D/Bola.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package tugasakhir.G3D;
+import tugasakhir.Benda3D;
+import tugasakhir.G2D.Lingkaran;
 
 /**
  *
  * @author abaym
  */
-public class Bola {
-    
+public class Bola extends Lingkaran implements Benda3D{
+    public Bola(double jariJari) {
+        super(jariJari);
+    }
+    @Override
+    public double menghitungVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(getJariJari(), 3);
+    }
+    @Override
+    public double menghitungLuasPermukaan() {
+        return 4 * Math.PI * Math.pow(getJariJari(), 2);
+    }
 }
