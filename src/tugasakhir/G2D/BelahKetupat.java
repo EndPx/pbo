02993d9c3703126bcef9f7a61121fe551a6b37ac
Fiuw Winlye <142503944+RusterF@users.noreply.github.com>
2024-05-10 22:06:06 +0700

    Update BelahKetupat.java

--- src/tugasakhir/G2D/BelahKetupat.java
+++ src/tugasakhir/G2D/BelahKetupat.java
@@ -23,4 +23,9 @@ public class BelahKetupat implements Benda2D {
     public double menghitungLuas() {
         return 0.5 * this.d1 * this.d2;
     }
+
+    @Override
+    public double menghitungKeliling() {
+        return 2 * (this.d1 + this.d2);
+    }
 }
