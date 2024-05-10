9af4763a573e20b1d614be8f77010a41c302db35
EndPx <127120051+EndPx@users.noreply.github.com>
2024-05-10 10:23:17 +0700

    Update Segitiga.java

--- src/tugasakhir/G2D/Segitiga.java
+++ src/tugasakhir/G2D/Segitiga.java
@@ -12,25 +12,22 @@ import tugasakhir.Benda2D;
  * @author abaym
  */
 public class Segitiga implements Benda2D {
-    private double alas;
-    private double tinggi;
-    private double luas;
+    public double alas;
+    public double tinggi;
+    public static double luasSegitiga;
 
     public Segitiga(double alas, double tinggi){
         this.alas = alas;
         this.tinggi = tinggi;
     }
 
-    public double getAlas(){
-        return alas;
-    }
-    public double getTinggi(){
-        return tinggi;
-    }
-
     @Override
     public double menghitungLuas() {
-        return 0.5 * alas * tinggi;
+        luasSegitiga = 0.5 * alas * tinggi;
+        return luasSegitiga;
     }
     
+    public double menghitungLuas(double alas, double tinggi){
+        return 0.5 * alas * tinggi;
+    }
 }
