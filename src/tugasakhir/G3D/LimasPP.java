bce307b1b8a1560df067c16cec92a0d1aebd4be4
Fiuw Winlye <142503944+RusterF@users.noreply.github.com>
2024-05-10 21:03:11 +0700

    Update LimasPP.java

--- src/tugasakhir/G3D/LimasPP.java
+++ src/tugasakhir/G3D/LimasPP.java
@@ -37,7 +37,7 @@ public class LimasPP extends PersegiPanjang implements Benda3D {
         return PersegiPanjang.luasPersegiPanjang + luasSelimut1 + luasSelimut2;
     }
 
-    public double menghitungLuasPermukaan(double luasAlas) {
+    public double menghitungLuasPermukaan(double panjang, double lebar) {
         double sisiTegak1 = Math.sqrt(Math.pow(0.5 * super.panjang,2) + Math.pow(tinggiLimasPP,2));
         double luasSelimut1 = 0.5 * sisiTegak1 * super.panjang * 2;
         double sisiTegak2 = Math.sqrt(Math.pow(0.5 * super.lebar,2) + Math.pow(tinggiLimasPP,2));
