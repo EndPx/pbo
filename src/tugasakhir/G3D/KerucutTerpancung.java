0e53d2c07c8e9992ceb8da06e7106854457e0686
RafiqIkhwan <90625702+RafiqIkhwan@users.noreply.github.com>
2024-05-11 10:34:05 +0700

    Update KerucutTerpancung.java
    dah lahh

--- src/tugasakhir/G3D/KerucutTerpancung.java
+++ src/tugasakhir/G3D/KerucutTerpancung.java
@@ -1,42 +1,48 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tugasakhir.G3D;
 
 import tugasakhir.Benda3D;
 
 /**
- *
- * @author abaym
- */
+ *
+ * @author abaym
+ */
 public class KerucutTerpancung extends Kerucut implements Benda3D {
-    private double jarijariKecil;
-    private double tinggiKerucutTerpancung;
-    public KerucutTerpancung(double jariJari, double tinggiKerucutTerpancung, double jarijariKecil) {
-        super(jariJari);
-        this.jarijariKecil = jarijariKecil;
-        this.tinggiKerucutTerpancung = tinggiKerucutTerpancung;
-    }
-    
-    @Override
-    public double menghitungVolume() {
-        return (1.0/3) * Math.PI * tinggiKerucutTerpancung * ((Math.pow(super.getJariJari(),2)) + (super.getJariJari() * jarijariKecil) + (Math.pow(jarijariKecil,2)));
-    }
-    
-    @Override
-    public double menghitungLuasPermukaan() {
-        return menghitungLuas() + menghitungLuas(jarijariKecil) + Math.PI * tinggiKerucutTerpancung * (getJariJari() + jarijariKecil);
-    }
-    
-    @Override
-    public double menghitungLuasPermukaan(double luasAlasBawah) {
-    double tinggi = tinggiKerucutTerpancung;
-    double rAtas = super.getJariJari();
-    double rBawah = jarijariKecil;
-    double sisiMiring = Math.sqrt(Math.pow(rBawah - rAtas, 2) + tinggi * tinggi);
-    double luasPermukaan = Math.PI * sisiMiring * (rAtas + rBawah) + luasAlasBawah;
-    return luasPermukaan;
-    }
+
+  public double jarijariKecil, tinggiKerucutKecil;
+  public static double volumeKerucutTerpancung;
+  public static double luasPermukaanKerucutTerpancung;
+
+  public KerucutTerpancung(double jariJari, double tinggiKerucut, double jarijariKecil, double tinggiKerucutKecil) {
+    super(jariJari, tinggiKerucut);
+    this.jarijariKecil = jarijariKecil;
+    this.tinggiKerucutKecil = tinggiKerucutKecil;
+  }
+
+  @Override
+  public double menghitungVolume() {
+    //volumeKerucutTerpancung = Kerucut.volumeKerucut + super.getJariJari() * jarijariKecil + Math.pow(jarijariKecil, 2);
+    volumeKerucutTerpancung = Kerucut.volumeKerucut - super.menghitungVolume(jarijariKecil, tinggiKerucutKecil);
+    return volumeKerucutTerpancung;
+  }
+
+  public double menghitungVolume(double jarijariBesar, double T, double jarijariKecil, double t) {
+    //return super.menghitungVolume(jarijariBesar, T) + jarijariBesar * jarijariKecil + Math.pow(jarijariKecil, 2);
+    return super.menghitungVolume(jarijariBesar,T) - super.menghitungVolume(jarijariKecil, t);
+  }
+
+  @Override
+  public double menghitungLuasPermukaan() {
+
+    luasPermukaanKerucutTerpancung = Kerucut.luasPermukaanKerucut - super.menghitungLuasPermukaan(jarijariKecil, tinggiKerucutKecil) + super.menghitungLuas(jarijariKecil);
+    return luasPermukaanKerucutTerpancung;
+  }
+
+  public double menghitungLuasPermukaan(double jarijariBesar, double T , double jarijariKecil, double t) {
+    return super.menghitungLuasPermukaan(jarijariBesar, T) - super.menghitungLuasPermukaan(jarijariKecil, t) + super.menghitungLuas(jarijariKecil);
+  }
 }
